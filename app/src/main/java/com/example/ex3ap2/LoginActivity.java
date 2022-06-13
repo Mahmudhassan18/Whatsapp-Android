c64370a640feff3c08688d11cf01ac6c9d248fea
Mahmudhassan <mahmud.hassan@live.biu.ac.il>
2022-06-14 01:14:51 +0300

    Added Add-contacts Activity.

--- app/src/main/java/com/example/ex3ap2/LoginActivity.java
+++ app/src/main/java/com/example/ex3ap2/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.ex3ap2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginSignupBtn = findViewById(R.id.loginSignupBtn);
+        loginSignupBtn.setOnClickListener(view -> {
+            Intent signUpIntent = new Intent(this, SignupActivity.class);
+            startActivity(signUpIntent);
+        });
     }
 }
--- app/src/main/java/com/example/ex3ap2/SignupActivity.java
+++ app/src/main/java/com/example/ex3ap2/SignupActivity.java
@@ -1,8 +1,10 @@
 package com.example.ex3ap2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button signupLoginBtn = findViewById(R.id.signupLoginBtn);
+        signupLoginBtn.setOnClickListener(view -> {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+        });
     }
 }
